c6319b44f6302483db139994da5663f986d166f9
PzoHua <46344364+PzoHua@users.noreply.github.com>
2019-08-13 21:51:12 +0800

    Update ex5.py

--- ex5.py
+++ ex5.py
@@ -10,6 +10,7 @@ my_hair = 'Brown'
 print "Let's talk about %s." % my_name
 print "He's %d inches tall." % my_height
 print "He's %d pounds heavy." % my_weight
+print "He's got %s eyes and %s hair." % (my_eyes,my_hair)
 print "Actually that's not too heavy."
 print "His teeth are usually %s depending on the coffee." % my_teeth
 # this line is tricky,try to get it exactly right
