1176e4fadbf25618ca3c1a59e7e31802bf2d1dc7
PzoHua <46344364+PzoHua@users.noreply.github.com>
2019-08-13 17:42:06 +0800

    Create ex3.py

---
+++ ex3.py
@@ -0,0 +1,28 @@
+#put word
+print "I will now count my chickens:"
+#calculate
+print "Hens", 25+30/6
+#tell users
+print "Now I will count the eggs:"
+#amount operation
+print "Roosters",100-25*3%4.0
+#count
+print 3+2+1-5+4%2-1/4.0+6
+#True or False
+print "It is true that 3+2<5-7?"
+#true or false by logical operation
+print 3+2<5-7
+#apart to count by logical operation
+print "What is 3+2?",3+2
+#Negative operation
+print "What is  5-7?",5-7
+#show errors
+print "Oh ,that's why it's False."
+#operation more again
+print "How about some more."
+#how to get True
+print "Is it greater?",5>-2
+#get ture by logical operation
+print "Is it greater or equal?",5>=-2
+#get False
+print "Is it less or equal?",5<=-2
