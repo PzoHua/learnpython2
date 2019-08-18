b8df2aa31d4b3e20bb1ceb7f749e1d8c928b6470
PzoHua <46344364+PzoHua@users.noreply.github.com>
2019-08-18 20:47:42 +0800

    Create ex10.py

---
+++ ex10.py
@@ -0,0 +1,19 @@
+
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat = """
+I'll do a list:
+\t* Cat foot
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+
+print tabby_cat
+print persian_cat
+print backslash_cat
+print fat_cat
+
+
+#转义字符的运用，练习常见的转义序列。
