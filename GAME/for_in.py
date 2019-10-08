502887afebd6e1c79c09d10836649a22bfcef14a
Puzhonghua <46344364+PzoHua@users.noreply.github.com>
2019-10-09 00:05:18 +0800

    Create for_in.py

---
+++ GAME/for_in.py
@@ -0,0 +1,12 @@
+#_*_coding=utf-8_*_
+
+# 练习for循环
+
+for letter in 'Python':
+	print '当前字母：', letter
+	
+fruits = ['banana', 'apple', 'mango']
+for fruit in fruits:
+	print '当前水果', fruit
+	
+print "Goodbye!" 
